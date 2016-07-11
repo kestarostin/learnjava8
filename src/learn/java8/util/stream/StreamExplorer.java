a13fbaf188d8db0937675c639b491347b0135a7e
selvindante <selvindante@gmail.com>
2016-07-11 22:33:33 +0300

    Added some stream creation examples

--- src/learn/java8/util/stream/StreamExplorer.java
+++ src/learn/java8/util/stream/StreamExplorer.java
@@ -38,10 +38,11 @@ public class StreamExplorer {
 	 * Prints first element of collection if exists using stream API.
 	 */
 	public static void printFirstElementWithStreams() {
-		Stream.of(STRING_NUMBERS_ARRAY)
-				.findFirst()
-				.ifPresent(System.out::println);
-	}
-
-
+        //Arrays.asList(STRING_NUMBERS_ARRAY).stream()
+        //Arrays.stream(STRING_NUMBERS_ARRAY)
+        //Stream.builder().add(STRING_NUMBERS_ARRAY[0]).add(STRING_NUMBERS_ARRAY[1]).add(STRING_NUMBERS_ARRAY[2]).build()
+        Stream.of(STRING_NUMBERS_ARRAY)
+                .findFirst()
+                .ifPresent(System.out::println);
+    }
 }
